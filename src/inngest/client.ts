0fff0dcc8da55431cf66e8f85b80d9087d6402c2
Musheer0 <musheeran165@gmail.com>
2026-08-14 23:02:39 +0530

    add inngest siging key

--- src/inngest/client.ts
+++ src/inngest/client.ts
@@ -1,4 +1,4 @@
 // src/inngest/client.ts
 import { Inngest } from "inngest";
 
-export const inngest = new Inngest({ id: "blur-field" });
+export const inngest = new Inngest({ id: "blur-field" ,eventKey:process.env.INNGEST_EVENT_KEY!});
